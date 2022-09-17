a09f09726c81585e56b766ef8662e65f7758ea50
junki <junki646@gmail.com>
2022-09-17 17:35:32 +0900

    Add : 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const up = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const down = () => {
+    setCount((prev) => prev - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={up}>up</button>
+      <button onClick={down}>down</button>
     </div>
   );
 }
